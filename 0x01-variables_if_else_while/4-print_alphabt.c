4c3258fd4eff7d9aafcea96deabf2713b51af23c
Leniors <mokualeeroy@gmail.com>
2023-03-17 11:52:59 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		print("%c", letter);
+		letter++;
+		if (letter == e || letter == q)
+		{
+			continue
+		}
+	return (0);
+}
