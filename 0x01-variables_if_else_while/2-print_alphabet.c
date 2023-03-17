3bff7a8c5e2a2d6c335f17d605d15c421da54e67
Leniors <mokualeeroy@gmail.com>
2023-03-17 08:50:25 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char letter = 'a';
+	while (letter <= z)
+	{
+		printf("%c", letter);
+		letter++;
+	}
+	return (0);
+}
