7541821e93d9ee429f37515227fde7588330ca4f
Leniors <mokualeeroy@gmail.com>
2023-03-17 07:36:22 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,13 +4,13 @@
 /* more headers goes there */
 
 /* betty style doc for function main goes there */
-int main(void)
-
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
+
+int main(void)
 {
 	int n;
 
