9973b678c4229b867ce845eba6ba868bb4495e6c
Leniors <mokualeeroy@gmail.com>
2023-03-17 16:59:36 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -19,7 +19,7 @@ int main(void)
 				putchar(' ');
 				putchar(j / 10 + '0');
 				putchar(j % 10 + '0');
-				if (i != 99 || j != 98)
+				if (i != 99 && j != 98)
 				{
 					putchar(',');
 					putchar(' ');
@@ -27,5 +27,6 @@ int main(void)
 			}
 		}
 	}
+	putchar('\n');
 	return (0);
 }
