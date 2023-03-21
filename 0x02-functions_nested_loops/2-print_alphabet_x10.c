bfe2236a4cbecab768a66c31bc699f14375698af
Leniors <mokualeeroy@gmail.com>
2023-03-21 08:41:10 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints lowwercase letters 10 times.
+ *
+ * returns nothing
+ */
+void print_alphabet_x10(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 10; i++)
+	{
+		j = 97;
+		while (j <= 122)
+		{
+			_putchar(j);
+			j++;
+		}
+		i++;
+	}
+	_putchar('\n';
+}
