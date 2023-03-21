39be59dcd8f183065fa19328fb4ebfda4124165c
Leniors <mokualeeroy@gmail.com>
2023-03-21 08:05:54 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int letter = 97;
+
+	while (letter <= 122)
+	{
+		_putchar(letter);
+	}
+	_putchar('\n');
+	return (0);
+}
