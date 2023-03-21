92e8a9691381ad19fbdf91476bf042f857146fd1
Leniors <mokualeeroy@gmail.com>
2023-03-21 14:34:23 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -17,10 +17,8 @@ void jack_bauer(void)
 			{
 				for (d = 0; d <= 9; d++)
 				{
-					if (b == 2 && c == 5)
-					{
-						return;
-					}
+					if (a >= 2 &&  b== 4)
+						break;
 					_putchar(a + 48);
 					_putchar(b + 48);
 					_putchar(58);
