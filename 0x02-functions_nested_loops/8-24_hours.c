1dd7b246dd5f678d0a0f0fe522053e509283f443
Leniors <mokualeeroy@gmail.com>
2023-03-21 14:01:02 -0700

    8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+void jack_bauer(void)
+{
+	int a, b, c, d;
+
+	for (a = 0; a <= 2; a++)
+	{
+		for (b = 0: b <= 3; b++)
+		{
+			for (c = 0; b <= 5; b+=)
+			{
+				for (d = 0; d <= 9; d++)
+				{
+					_putchar(a + 48);
+					_putchar(b + 48);
+					_putchar(58);
+					_putchar(c + 48);
+					_putchar(d + 48);
+				}
+			}
+		}
+	}
+}
+
