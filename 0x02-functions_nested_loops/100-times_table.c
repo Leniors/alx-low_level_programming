aeea4514cd99f8e27c63d08a05be011c20adbd98
Leniors <mokualeeroy@gmail.com>
2023-03-21 16:36:16 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -17,7 +17,7 @@ void print_times_table(int n)
 				k = j * i;
 				if(j == 0)
 				{
-					_putchar(k + 48);
+					_putchar(k + '0');
 				}
 				else if (k < 10 && j != 0)
 				{
@@ -25,23 +25,23 @@ void print_times_table(int n)
 					_putchar(' ');
 					_putchar(' ');
 					_putchar(' ');
-					_putchar(k + 48);
+					_putchar(k + '0');
 				}
 				else if (k >= 10 && k < 100)
 				{
 					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
-					_putchar((k / 10) + 48);
-					_putchar((k % 10) + 48);
+					_putchar((k / 10) + '0');
+					_putchar((k % 10) + '0');
 				}
 				else if (k >= 100)
 				{
 					_putchar(',');
 					_putchar(' ');
-					_putchar((k / 100) + 48);
-					_putchar(((k / 10) % 10) + 48);
-					_putchar((k % 10) + 48);
+					_putchar((k / 100) + '0');
+					_putchar(((k / 10) % 10) + '0');
+					_putchar((k % 10) + '0');
 				}
 			}
 			_putchar('\n');
