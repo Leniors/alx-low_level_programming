00c7af34b3d4ec9bc57c5d329ba50391f453f97f
Leniors <mokualeeroy@gmail.com>
2023-03-21 15:27:58 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,25 +11,28 @@ void times_table(void)
 	for (i = 0; i < 10; i++)
 	{
 		for (j = 0; j < 10; j++)
+		{
+			
 			k = j * i;
-		if (j == 0)
-		{
-			_putchar(k + 48);
+			if (j == 0)
+			{
+				_putchar(k + 48);
+			}
+			if (k < 10 && j != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(k + 48);
+			}
+			else if (k >= 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar((k / 10) + 48);
+				_putchar((k % 10) + 48);
+			}
 		}
-		if (k < 10 && j != 0)
-		{
-			_putchar(',');
-			_putchar(' ');
-			_putchar(' ');
-			_putchar(k + 48);
-		}
-		else if (k >= 10)
-		{
-			_putchar(',');
-			_putchar(' ');
-			_putchar((k / 10) + 48);
-			_putchar((k % 10) + 48);
-		}
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
