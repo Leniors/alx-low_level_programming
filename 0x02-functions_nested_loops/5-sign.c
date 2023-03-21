d9ddaa890461696864098c3b10e88000e8a7aa0f
Leniors <mokualeeroy@gmail.com>
2023-03-21 12:05:30 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,17 +11,18 @@ int print_sign(int n)
 {
 	if (n >= 1)
 	{
-		_putchar('+');
+		_putchar(43);
 		return (1);
 	}
 	else if (n <= -1)
 	{
-		_putchar('-');
+		_putchar(45);
 		return (-1);
 	}
 	else if (n == 0)
 	{
-		_putchar('0');
+		_putchar(48);
 		return (0);
 	}
+	_putchar('\n');
 }
