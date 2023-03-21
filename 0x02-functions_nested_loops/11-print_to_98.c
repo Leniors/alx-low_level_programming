6f8b64cef9bd45bfc848398ccc259ed1221e0edb
Leniors <mokualeeroy@gmail.com>
2023-03-21 16:03:29 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,14 +12,14 @@ void print_to_98(int n)
 	{
 		for (i = n; i <= 98; i++)
 		{
-			printf("%d, ", i);
+			printf("%d", i);
 		}
 	}
 	else if (n >= 98)
 	{
 		for (j = n; j >= 98; j--)
 		{
-			printf("%d, ", j);
+			printf("%d", j);
 		}
 	}
 }
