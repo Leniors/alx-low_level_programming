37b1cf26cf07ef39d4ce132f58944d115bf1da86
Leniors <mokualeeroy@gmail.com>
2023-03-21 12:29:49 -0700

    7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints last digit i.
+ *
+ * @i : number being evaluated.
+ *
+ * Return: Always 0.
+ */
+int print_last_digit(int i)
+{
+	return (i % 10);
+}
