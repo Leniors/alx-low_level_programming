9c03bd5430e77ae6926aae0507f95d718fad8cfa
Leniors <mokualeeroy@gmail.com>
2023-03-23 14:28:05 -0700

    ./8-squares

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_square - draws square
+ * @size: size of square
+ *
+ * Return: always 0
+ */
+void print_square(int size)
+{
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int i, j;
+
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+			{
+				_putchar(35);
+			}
+			_putchar('\n');
+		}
+	}
+}
