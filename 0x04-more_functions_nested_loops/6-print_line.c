716b97d205d67ffde8330fc37ea96c200f44a74e
Leniors <mokualeeroy@gmail.com>
2023-03-23 11:18:07 -0700

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_line - prints a line n times
+ * @n: number used
+ *
+ * Return: alwys 0
+ */
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i <= n; i++)
+	{
+		_putchar(95);
+	}
+	_putchar('\n');
+}
