358249c26b0d8ae6f3946f37cd4fc1a337efac55
Leniors <mokualeeroy@gmail.com>
2023-03-23 15:56:07 -0700

    100-prime_factor.c

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -17,7 +17,7 @@ int main(void)
 	while (n % 2 == 0)
 	{
 		max = 2;
-		n /= 2;
+		n = n / 2;
 	}
 	for (i = 3; i <= sqrt(n); i = i + 2)
 	{
@@ -28,9 +28,7 @@ int main(void)
 		}
 	}
 	if (n > 2)
-	{
 		max = n;
-	}
 	printf("%ld\n", max);
 	return (0);
 }
