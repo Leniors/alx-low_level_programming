be683fb61f0e9b9ceae957d444314b76a02d9ee4
Leniors <mokualeeroy@gmail.com>
2023-03-23 16:09:14 -0700

    101-print_number.c

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_number - prints a num
+ *
+ * @n: integer printed
+ */
+void print_number(int n)
+{
+	unsigned int i;
+
+	if (n < 0)
+	{
+		i = -n;
+		putchar('-');
+	}
+	else
+	{
+		i = n;
+	}
+	if (i / 10)
+	{
+		print_number(i / 10);
+	}
+
+	_putchar((i % 10) + 48);
+}
