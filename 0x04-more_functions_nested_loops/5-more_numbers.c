0df2ae9cd05c8b654b768f33deadb1dcc32aed71
Leniors <mokualeeroy@gmail.com>
2023-03-23 09:10:57 -0700

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -14,11 +14,11 @@ void more_numbers(void)
 	{
 		for (i = 0; i < 15; i++)
 		{
-			if (i > 9)
+			if (i >= 10)
 			{
 				_putchar('1');
 			}
-			_putchar(48 + i % 10);
+			_putchar(i % 10 + '0');
 		}
 		k++;
 	}
