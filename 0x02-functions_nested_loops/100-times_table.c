70033907d059180d0664550831e4c8728b207d02
Leniors <mokualeeroy@gmail.com>
2023-03-26 06:50:29 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -12,49 +12,60 @@ void print_times_table(int n)
 		return;
 	for (i = 0; i <= n; i++)
 	{
-		for (j = 0; j <= n; j++)
+		if (i == 0)
 		{
-			int k;
-
-			k = i * j;
-			if (j == 0)
-			{
-				_putchar(48);
-			}
-			if (k > 0 && k <= 9)
-			{
-				if (j < n)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(' ');
-				}
-				_putchar(48 + k);
-			}
-			else if (k > 9 && k < 100)
-			{
-				if (j < n)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-				}
-				_putchar(48 + (k / 10));
-				_putchar(48 + (k % 10));
-			}
-			else if (k >= 100)
-			{
-				if (j < n)
-				{
-					_putchar(',');
-					_putchar(' ');
-				}
-				_putchar(48 + k / 100);
-				_putchar(48 + (k % 100) / 10);
-				_putchar(48 + k % 10);
-			}
+			_putchar(48);
+			_putchar(',');
+			_putchar(' ');
+			_putchar(' ');
+			_putchar(' ');
 		}
-		_putchar('\n');
+		if ( i > 0)
+		{
+			for (j = 0; j <= n; j++)
+			{
+				int k;
+				
+				k = i * j;
+				if (k == 0)
+				{
+					_putchar(48);
+				}
+				if (k > 0 && k <= 9)
+				{
+					if (j < n)
+					{
+						_putchar(',');
+						_putchar(' ');
+						_putchar(' ');
+						_putchar(' ');
+					}
+					_putchar(48 + k);
+				}
+				else if (k > 9 && k < 100)
+				{
+					if (j < n)
+					{
+						_putchar(',');
+						_putchar(' ');
+						_putchar(' ');
+					}
+					_putchar(48 + (k / 10));
+					_putchar(48 + (k % 10));
+				}
+				else if (k >= 100)
+				{
+					if (j < n)
+					{
+						_putchar(',');
+						_putchar(' ');
+					}
+					_putchar(48 + k / 100);
+					_putchar(48 + (k % 100) / 10);
+					_putchar(48 + k % 10);
+				}
+			}
+			_putchar('\n');
+		}
 	}
 }
