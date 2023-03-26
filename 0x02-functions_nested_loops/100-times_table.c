30dcfab7ff01f817e158a2341700df0cb345a663
Leniors <mokualeeroy@gmail.com>
2023-03-26 05:46:46 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -36,7 +36,6 @@ void print_times_table(int n)
 				{
 					_putchar(',');
 					_putchar(' ');
-					_putchar(' ');
 				}
 			}
 			else
@@ -47,6 +46,7 @@ void print_times_table(int n)
 				if (j < n)
 				{
 					_putchar(',');
+					_putchar(' ');
 					_putchar(' ');
 				}
 			}
