cf5b1b96f79929453c984d5699fc5f7308f592f2
Leniors <mokualeeroy@gmail.com>
2023-03-27 13:52:22 -0700

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-main.c
@@ -0,0 +1,16 @@
+julien@ubuntu:~/0x05$ cat 6-main.c
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char *str;
+
+	str = "0123456789";
+	puts2(str);
+	return (0);
+}
