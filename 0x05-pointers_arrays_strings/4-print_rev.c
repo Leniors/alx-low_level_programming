1486bb98cc31241c6a57470d7dc1c342c5b308cb
Leniors <mokualeeroy@gmail.com>
2023-03-27 13:08:43 -0700

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,8 +6,12 @@
  */
 void print_rev(char *s)
 {
-	while (*s == '\n')
+	int i, len;
+
+	for (len = 0; s[len] != '\0'; len++);
+	for (i = len - 1; i >= 0; i--)
 	{
-		_putchar(*s--);
+		_putchar(s[i]);
 	}
+	_putchar('\n');
 }
