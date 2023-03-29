8ec6c82a0d36ebc233df646768f07aacc6674bfd
Leniors <mokualeeroy@gmail.com>
2023-03-29 16:22:05 -0700

    ./2-strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -9,11 +9,18 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int j;
+	int i;
 
-	for (j = 0; j < n; j++)
+	i = 0;
+	while (i < n && src[i] != '\0')
 	{
-		dest[j] = src[j];
+		dest[i] = src[i];
+		i++;
+	}
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
 	}
 	return (dest);
 }
