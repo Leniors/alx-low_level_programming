7d6c10ac0372f34225ecb7575c1ebb40b978dcfc
Leniors <mokualeeroy@gmail.com>
2023-03-30 11:52:33 -0700

    ./6-cap

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * cap_string - changes string to uppercase
+ * @s: string
+ * Return: returns a string
+ */
+char *cap_string(char *s)
+{
+	int i;
+	int j;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	j = 0;
+	while (j < i)
+	{
+		if (s[j] == ' ' || s[j] == '.' || s[j] == '\n' || s[j] == ',' || s[j] == '!' || s[j] == '\t')
+		{
+			s[j + 1] = toupper(s[j + 1]);
+		}
+		j++;
+	}
+	return (s);
+}
