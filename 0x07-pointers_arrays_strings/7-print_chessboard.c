d2ce61175a53609ad24a3069bce0cfa3b54a5486
Leniors <mokualeeroy@gmail.com>
2023-04-03 14:12:37 -0700

    for (i = 0; i < 8; i++)

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,10 +1,19 @@
 #include "main.h"
 
 /**
- * print_chessboard - prints chess board
- * @a: variable
+ * print_chessboard - Entry point
+ * @a: array
+ * Return: Always 0 (Success)
  */
 void print_chessboard(char (*a)[8])
 {
-	return (a);
+	int i;
+	int j;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+			_putchar(a[i][j]);
+		_putchar('\n');
+	}
 }
