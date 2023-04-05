6f68ca573c316452dba52d4553b993cada440e74
Leniors <mokualeeroy@gmail.com>
2023-04-05 03:06:15 -0700

    task1

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - reveses a string
+ * @s: string
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+	if (*s == '\0')
+	{
+		_putchar('\n');
+	}
+}
