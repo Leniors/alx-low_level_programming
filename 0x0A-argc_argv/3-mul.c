0a3637cf1ee6600b2b48ac7d1fdfa2fd1d0dd267
Leniors <mokualeeroy@gmail.com>
2023-04-09 05:41:40 -0700

    mul

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - mainfunction
+ * @argc: count
+ * @argv: v
+ * Return: always 0 succes
+ */
+int main(int argc, char *argv[])
+{
+	int mul;
+
+	if (argc > 1)
+	{
+		mul = argv[1] * argv[2];
+		printf("%d\n", mul);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+	return (0);
+}
