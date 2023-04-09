4dec8431039e8e937c0f2d48d2d27ffff379266e
Leniors <mokualeeroy@gmail.com>
2023-04-09 05:05:00 -0700

    argv count

---
+++ 0x0A-argc_argv/1-argc.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ * @argc: count
+ * @argv: v
+ * Return: always 0 successs
+ */
+int main(int argc, char *argv)
+{
+	(void)argv;
+
+	printf("%d", argc);
+	return (0);
+}
