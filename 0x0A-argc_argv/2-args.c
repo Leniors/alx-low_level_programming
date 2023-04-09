34069727bc1c9a9c7e360a3791c82ed0b4b9a08f
Leniors <mokualeeroy@gmail.com>
2023-04-09 05:24:08 -0700

    argv

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - mainfunction
+ * @argc: count
+ * @argv: array
+ * Return: always 0 success
+ */
+int main(int argc, char *argv)
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
