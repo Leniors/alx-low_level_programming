3d0bb80191437013450f21deb33cfaf3b61697bb
Leniors <mokualeeroy@gmail.com>
2023-04-09 05:29:12 -0700

    argv

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -6,13 +6,13 @@
  * @argv: array
  * Return: always 0 success
  */
-int main(int argc, char **argv)
+int main(int argc, char* argv)
 {
 	int i;
 
 	for (i = 0; i < argc; i++)
 	{
-		printf("%s\n", *argv[i]);
+		printf("%s\n", argv[i]);
 	}
 	return (0);
 }
