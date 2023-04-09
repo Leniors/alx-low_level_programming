c2a28b0eaa88c3e3473995c221cfd3e65a0a399e
Leniors <mokualeeroy@gmail.com>
2023-04-09 13:24:24 -0700

    add

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - mainfunction
+ * @argc: count
+ * @argv: v
+ * Return: always 0 success
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+	int sum = 0;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+	}
+	else if (argc > 1)
+	{
+		i = 0;
+		while (i < argc)
+		{
+			if (atoi(argv[i]) > 1000)
+			{
+				sum += atoi(argv[i]);
+			}
+			else
+			{
+				printf("Error");
+				return (1);
+			}
+			i++;
+		}
+		printf("%d", sum);
+	}
+	return (0);
+}
