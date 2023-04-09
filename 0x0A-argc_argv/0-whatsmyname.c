55e90ac355eec5005a4742753f421a9db4a0c376
Leniors <mokualeeroy@gmail.com>
2023-04-09 04:51:51 -0700

    whats my name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ * @argc: argument count
+ * @argv: argument v
+ * Return: 0 alwys success
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s", argv[0]);
+
+	return 0;
+}
