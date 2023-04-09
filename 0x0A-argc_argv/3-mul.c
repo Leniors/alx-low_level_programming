e67b22b4bba9759c3f377e66d89e6f8f9f3b534c
Leniors <mokualeeroy@gmail.com>
2023-04-09 11:54:08 -0700

    mul

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,11 +10,13 @@
 int main(int argc, char *argv[])
 {
 	int i;
+	int j;
 
-	if (argc > 1)
+	if (argc == 3)
 	{
-		i = atoi(argv[1]) * atoi(argv[2]);
-		printf("%d\n", i);
+		i = atoi(argv[1]);
+		j = atoi(argv[2];
+		printf("%d\n", i * j);
 	}
 	else
 	{
