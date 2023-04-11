7c7d12681f09c138cab0e93ebbc1945d0d42eaf7
Leniors <mokualeeroy@gmail.com>
2023-04-11 09:04:28 -0700

    duplicate

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+
+/**
+ * *_strdup - returns string
+ * @str: string
+ * Return: returns string
+ */
+char *_strdup(char *str)
+{
+	char *p;
+	int i;
+	
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	
+	p = malloc((i + 1) * sizeof(char));
+
+	for (j = 0; j < i; j++)
+	{
+		p[i] == str[i];
+	}
+	return (p);
+}
