d30f5a94af40e7b00addfe1dee7a1b2978a1d5c1
Leniors <mokualeeroy@gmail.com>
2023-04-13 05:03:39 -0700

    concatinate

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,10 +11,7 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *p;
-	unsigned int i;
-	unsigned int j;
-	unsigned int len1;
-	unsigned int len2;
+	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
 
 	if (s1 == NULL)
 	{
@@ -24,12 +21,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		s2 = "";
 	}
-	len1 = 0;
 	while (s1[len1] != '\0')
 	{
 		len1++;
 	}
-	len2 = 0;
 	while (s2[len2] != '\0')
 	{
 		len2++;
@@ -38,20 +33,14 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		n = len2;
 	}
-
 	p = malloc((len1 + n + 1) * sizeof(char));
 	if (p == NULL)
-	{
 		return (NULL);
-	}
-
-	i = 0;
 	while (s1[i] != '\0')
 	{
 		p[i] = s1[i];
 		i++;
 	}
-	j = 0;
 	while (s2[j] != '\0' && j <= n)
 	{
 		p[i] = s2[j];
