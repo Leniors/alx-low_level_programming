8ca004b478836a08c343289a82e0f19348968506
Leniors <mokualeeroy@gmail.com>
2023-04-13 04:39:25 -0700

    concatinate

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -34,8 +34,12 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		len2++;
 	}
+	if (n >= len2)
+	{
+		n = len2;
+	}
 
-	p = malloc(sizeof(char) * (len1 + len2 + 1));
+	p = malloc((len1 + n + 1) * sixeof(char));
 	if (p == NULL)
 	{
 		return (NULL);
