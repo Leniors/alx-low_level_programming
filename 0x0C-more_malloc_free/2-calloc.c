a595314134ddb718830121703180149ea2cd9b8e
Leniors <mokualeeroy@gmail.com>
2023-04-13 05:27:55 -0700

    calloc

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * *_calloc - allocates memory
+ * @nmemb: int
+ * @size: int
+ * Return: returns pointer
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *p;
+	char *ptr;
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	p = malloc(nmemb * size);
+	if (p == NULL)
+		return (NULL);
+	for (i = 0; i < nmeb * size; i ++)
+	{
+		*ptr = 0;
+	}
+}
