6efe84400a5c0f97fe19451a7ba9310d95723e44
Leniors <mokualeeroy@gmail.com>
2023-04-17 05:51:30 -0700

    new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -8,14 +8,14 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *dog;
+	dog_t *dn;
 
-	dog = malloc(sizeof(dog_t));
-	if (dog == NULL)
+	dn = malloc(sizeof(dog_t));
+	if (dn == NULL)
 	{
 		return (NULL);
 	}
-	dog->name = name;
-	dog->age = age;
-	dog->owner = owner;
+	dn->name = name;
+	dn->age = age;
+	dn->owner = owner;
 }
