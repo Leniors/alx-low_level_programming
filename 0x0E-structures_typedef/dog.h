d2a7afdc5cf407ecc03aa1e427fb28a33c2b6734
Leniors <mokualeeroy@gmail.com>
2023-04-17 02:50:39 -0700

    structure

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,13 +1,11 @@
 #ifndef DOG_H
 #define DOG_H
 
-static dog
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
-
-void init_dog(struct dog *d, char *name, float age, char *owner);
+}
 
 #endif
