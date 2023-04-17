9480e21db33eb71e223ab1c321568af76dc33c8d
Leniors <mokualeeroy@gmail.com>
2023-04-17 03:02:23 -0700

    structures

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,12 @@
 #ifndef DOG_H
 #define DOG_H
-
+/**
+ * struct dog - basic info
+ * @name: name
+ * @age: age
+ * @owner: owner
+ * Description: longer
+ */
 struct dog
 {
 	char *name;
@@ -9,3 +15,4 @@ struct dog
 }
 
 #endif
+
