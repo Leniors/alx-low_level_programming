41b53ada335dd7f3ef3270d17cf41c69546562ef
Leniors <mokualeeroy@gmail.com>
2023-04-19 05:04:55 -0700

    commiting

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -7,7 +7,7 @@
  * @size: elements@action: pointer
  * Return: void
  */
-void array_iterator(int *array, size_t size,void (action)(int))
+void array_iterator(int *array, size_t size, void (action)(int))
 {
 	unsigned int i;
 
