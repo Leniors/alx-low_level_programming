964f23897f48163039c89bd69672ffd1ad6a0309
Leniors <mokualeeroy@gmail.com>
2023-04-20 13:36:58 -0700

    sum

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,6 +1,9 @@
 #ifndef _VARIADIC_H
 #define _VARIADIC_H
 
+#include <stdio.h>
+#include <stdarg.h>
+
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print numbers(const char *separator, const unsigned int n, ...);
