e97c2201446c010810347a79e73876de6c5e7e1d
Leniors <mokualeeroy@gmail.com>
2023-04-20 14:05:58 -0700

    sums numbers

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -4,7 +4,6 @@
 /**
  * sum_them_all - sums
  * @n: numer
- * @...: varables
  * Return: always 0 is success
  */
 int sum_them_all(const unsigned int n, ...)
@@ -16,14 +15,10 @@ int sum_them_all(const unsigned int n, ...)
 	va_start(ap, n);
 
 	if (n == 0)
-	{
 		return (0);
-	}
 
 	for (i = 0; i < n; i++)
-	{
 		sum += va_arg(ap, int);
-	}
 
 	va_end(ap);
 
