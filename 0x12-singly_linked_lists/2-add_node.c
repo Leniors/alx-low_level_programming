b32ee8c3a82f2a36688c4ddc4665f34dec0c08d3
Leniors <mokualeeroy@gmail.com>
2023-04-27 12:04:52 -0700

    linked list

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -25,7 +25,7 @@ list_t *add_node(list_t **head, const char *str)
 	new->str = strdup(str);
 	new->len = len;
 	new->next = (*head);
-	(*head) = ne;
+	(*head) = new;
 
 	return (*head);
 }
--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -24,7 +24,7 @@ list_t *add_node_end(list_t **head, const char *str)
 		return (NULL);
 	}
 	new->str = strdup(str);
-	new-len = len;
+	new->len = len;
 	new->next = NULL;
 
 	if (*head == NULL)
