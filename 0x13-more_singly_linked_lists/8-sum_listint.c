2f3813ae1746e38b54e6431dfaaa581d3209e9f7
Leniors <mokualeeroy@gmail.com>
2023-05-01 14:54:36 -0700

    sum

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * sum_listint - sums a linked list
+ * @head: linked list
+ * Return: RETURNS SUM
+ */
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
+	listint_t *current;
+
+	if (head == NULL)
+	{
+		return (0);
+	}
+	current = head;
+	while (current)
+	{
+		sum += current;
+		current = current->next;
+	}
+	return (sum);
+}
