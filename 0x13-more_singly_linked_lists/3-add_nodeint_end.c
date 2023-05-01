bf90ae91c228ebee2cca8984e550eeecd64a84b3
Leniors <mokualeeroy@gmail.com>
2023-05-01 13:21:51 -0700

    adds

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -9,7 +9,7 @@
  */
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-	listint_t *last = head;
+	listint_t *last = *head;
 	listint_t *new_node;
 
 	new_node = malloc(sizeof(listint_t));
@@ -20,6 +20,11 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 
 	new_node->n = n;
 	new_node->next = NULL;
+	if (*head == NULL)
+	{
+		*head = new_node;
+		return (new_node);
+	}
 
 	while (last->next != NULL)
 	{
