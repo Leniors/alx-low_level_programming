f7c5a016a583165a78af27e1b5eea92a77cfaa94
Leniors <mokualeeroy@gmail.com>
2023-05-01 13:38:56 -0700

    frees

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -2,10 +2,13 @@
 #include <stdlib.h>
 
 /**
+ * free_listint - frees a linked list
  * @head: linked list
  */
 void free_listint(listint_t *head)
 {
+	listint_t temp;
+
 	while (head)
 	{
 		temp = head;
