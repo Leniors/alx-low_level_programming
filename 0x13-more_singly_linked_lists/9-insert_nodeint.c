a04ffb9b0af64865bb324806840a83af928a3a41
Leniors <mokualeeroy@gmail.com>
2023-05-02 00:31:41 -0700

    insert

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -26,6 +26,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		if (i == idx)
 		{
+			new->next = current->next;
 			current->n = new->n;
 		}
 		current = current->next;
