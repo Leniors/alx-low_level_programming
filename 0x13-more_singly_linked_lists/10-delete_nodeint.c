241dfb7b2b3d8f952257d359900b504ddab48050
Leniors <mokualeeroy@gmail.com>
2023-05-02 06:17:00 -0700

    delete

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -21,14 +21,14 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	{
 		*head = (*head)->next;
 		free(temp);
-		return (-1);
+		return (1);
 	}
 
 	while (i < index - 1)
 	{
 		if (!temp || (temp->next))
 		{
-			return (-1);
+			return (1);
 		}
 		temp = temp->next;
 		i++;
