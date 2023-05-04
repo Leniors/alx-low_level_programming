537851d59cc1f392c9eee7f580e4f80c3b769e87
Leniors <mokualeeroy@gmail.com>
2023-05-04 11:14:16 -0700

    print

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_binary - prints
+ * @n: number
+ */
+void print_binary(unsigned long int n)
+{
+	int i ;
+	unsigned long int mask;
+
+	mask -= 1UL << (sizeof(unsigned long int) * 8 - 1);
+	for (i = 0; i < sizeof(unsigned long iint) * 8; i++)
+	{
+		if (n & mask)
+		{
+			_putchar('1');
+		}
+		else
+		{
+			_putchar('0');
+		}
+		mask >>= 1;
+	}
+	_putchar('\n');
+}
