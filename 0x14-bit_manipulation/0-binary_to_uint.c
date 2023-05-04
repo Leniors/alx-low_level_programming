808b26b91d2a5fbdb9c4022397f9934ecc92d50a
Leniors <mokualeeroy@gmail.com>
2023-05-04 10:08:22 -0700

    convert

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -7,7 +7,7 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int i;
+	unsigned int i = 0;
 
 	if (b == NULL)
 	{
@@ -15,11 +15,12 @@ unsigned int binary_to_uint(const char *b)
 	}
 	while (*b != '\0')
 	{
-		if (*b != '0' || *b != '1')
+		if (*b < '0' || *b > '1')
 		{
 			return (0);
 		}
 		i = (i << 1) + (*b - '0');
+		b++;
 	}
 	return (i);
 }
