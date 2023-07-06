a6c9c98e3a4440bfb3a78c6439dfc019f18cdc95
Leniors <mokualeeroy@gmail.com>
2023-07-06 13:07:18 -0700

    set bit

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -10,11 +10,6 @@ int set_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned long int mask = 1 << index;
 
-	if (*n != (NULL))
-	{
-		*n = (*n | mask);
-		return (1);
-	}
-	else
-		return (0);
+	*n = (*n | mask);
+	return (1);
 }
