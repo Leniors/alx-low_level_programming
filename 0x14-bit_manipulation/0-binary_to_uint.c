745f16e0c128a07203952b4ddd9b9df0a654a197
Leniors <mokualeeroy@gmail.com>
2023-07-06 11:52:15 -0700

    print binary

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -5,25 +5,29 @@
  * b: string
  * Return: returns uint
  */
-unsigned int binary_to_uint(const char *b) {
+unsigned int binary_to_uint(const char *b)
+{
 	unsigned int len = 0;
 	unsigned int decimal = 0;
 	unsigned int i;
 	unsigned int power;
 	unsigned int j;
 
-	while (b[len] != '\0') {
-		if (b[len] != '1' && b[len] != '0') {
+	while (b[len] != '\0')
+	{
+		if (b[len] != '1' && b[len] != '0')
 			return 0;
-		}
 		len++;
 	}
 
-	for (i = 0; i < len; i++) {
-		if (b[i] == '1') {
-			power = len -1 - i;
+	for (i = 0; i < len; i++)
+	{
+		if (b[i] == '1')
+		{
+			power = len - 1 - i;
 			j = 1;
-			while (power > 0) {
+			while (power > 0)
+			{
 				j *= 2;
 				power--;
 			}
--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -4,11 +4,11 @@
  * print_binary - function
  * n: unsigned int
  */
-void print_binary(unsigned long int n) {
+void print_binary(unsigned long int n)
+{
 	unsigned int mask = 1;
 
-	if (n > 1) {
+	if (n > 1)
 		print_binary(n >> 1);
-	}
-	_putchar(n & mask ? '1': '0');
+	_putchar(n & mask ? '1' : '0');
 }
