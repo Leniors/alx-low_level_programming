2dcce9add8750c55bf97ecfe2f536197d64b35a2
Leniors <mokualeeroy@gmail.com>
2023-07-06 16:44:26 -0700

    flip

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,20 @@
+#include "min.h"
+
+/**
+ * flip_bits - function
+ * @n: int
+ * @m: int
+ * Return: returns
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int differ = n ^ m;
+	unsigned int count = 0;
+
+	while (differ != 0)
+	{
+		count += differ & 1;
+		differ >>= 1;
+	}
+	return (count);
+}
