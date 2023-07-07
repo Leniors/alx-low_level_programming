d946beff8b43ab5eaf632febe0d172c92a4c8ce1
Leniors <mokualeeroy@gmail.com>
2023-07-06 17:04:06 -0700

    endianness

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -4,10 +4,10 @@
  * get_endianness - function
  * Return: returns integer
  */
-int get_endinnes(voi)
+int get_endianness(void)
 {
 	unsigned int value = 1;
 	char *byte_pointer = (char*) & value;
 
-	return (int) (*byte_pointer);
+	return ((int) (*byte_pointer));
 }
