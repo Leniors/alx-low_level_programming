be0bb6ea143454118ffa93ed07ac7049ef93c8aa
Leniors <mokualeeroy@gmail.com>
2023-07-10 10:49:25 -0700

    create

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,7 +14,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	while (*text_content != NULL)
+	while (text_content != NULL)
 	{
 		len++;
 	}
@@ -27,6 +27,7 @@ int create_file(const char *filename, char *text_content)
 		close(fd);
 		return (-1);
 	}
+
 	close(fd);
 	return (1);
 }
