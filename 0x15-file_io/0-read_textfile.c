803c976db4b451593ce187d866d7b4c1ba016c80
Leniors <mokualeeroy@gmail.com>
2023-07-10 08:40:56 -0700

    reading a file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <unistd.h>
 
 /**
  * read_textfile - function
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,7 +1,6 @@
 #define MAIN_H
 #ifndef MAIN_H
 
-#include <unistd.h>
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 
