b0fac6279730d894a6880c071da7bf775ba5c0a4
Leniors <mokualeeroy@gmail.com>
2023-07-10 08:48:47 -0700

    red

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * read_textfile - function
+ * @filename: filename
+ * @letters: num of letters
+ * Return: returns size
+ */
+ssize_t read_textfile(const char *filename, size letters)
+{
+	ssize_t sizeread;
+	
+	return (sizeread);
+}
