53d5411f5d84a0ee56a7061099aef4bae3093a7a
Leniors <mokualeeroy@gmail.com>
2023-07-10 08:32:27 -0700

    reading a file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -10,11 +10,15 @@ ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
 	ssize_t sizeread;
+	char buffer[letters + 1];
 
 	fd = open(filename, O_RDONLY);
 	if (fd == -1 || *filename == NULL)
-		return 0;
-	sizeread = read(fd, STDOUT_FILENO);
+	{
+		close(fd);
+		return (0);
+	}
+	sizeread = read(fd, buffer, STDOUT_FILENO);
 	close(fd);
 	return (sizeread);
 }
