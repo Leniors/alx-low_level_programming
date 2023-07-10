9c0f3dca3f2ca09907d886d499c6d4db94555d8f
Leniors <mokualeeroy@gmail.com>
2023-07-10 10:17:52 -0700

    reading a file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -13,27 +13,31 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	ssize_t _read;
 	ssize_t written;
 
-	buffer = malloc((letters + 1) * sizeof(char));
 	if (filename == NULL)
 		return (0);
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
-	{
-		close(fd);
 		return (0);
-	}
+
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+		return (0);
+
 	_read = read(fd, buffer, letters);
 	if (_read == -1)
 	{
+		free(buffer);
 		close(fd);
 		return (0);
 	}
-	written = write(STDOUT_FILENO, buffer, letters);
-	if (writtn == -1)
+	written = write(STDOUT_FILENO, buffer, _read);
+	if (written == -1)
 	{
+		free(buffer);
 		close(fd);
 		return (0);
 	}
 
+	close(fd);
 	return (_read);
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -4,7 +4,9 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
-int _putchar(char c);
+#include <sys/types.h>
+#include <sys/stat.h>
+
 ssize_t read_textfile(const char *filename, size_t letters);
 
 #endif
