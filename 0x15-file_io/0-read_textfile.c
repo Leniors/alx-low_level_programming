9095331c9bdf5670dea5d37a5b6a28e485ba0011
Leniors <mokualeeroy@gmail.com>
2023-07-10 09:07:43 -0700

    reading a file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -8,7 +8,14 @@
  */
 ssize_t read_textfile(const char *filename, size letters)
 {
+	int fd;
 	ssize_t sizeread;
-	
+	char *buffer;
+
+	buffer = malloc(sizeof(char) * (letters + 1));
+	fd = open(filename, O_RDONLY);
+	sizeread = read(fd, filename, letters);
+	write(fd, STDOUT_FILENO, letters);
+
 	return (sizeread);
 }
