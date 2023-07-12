9e615fbdd665c3c3ccc8921b217968257ef3492a
Leniors <mokualeeroy@gmail.com>
2023-07-12 02:10:04 -0700

    copy

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -63,10 +63,9 @@ int main(int argc, char *argv[])
 	buffer = create_buffer(argv[2]);
 	from = open(argv[1], O_RDONLY);
 	r = read(from, buffer, 1024);
-	to = open(argv[2], O_CREAT | O_WRONLY | O-TRUNC, 0664);
+	to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 
-	do
-	{
+	do{
 		if (from == -1 || r == -1)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
