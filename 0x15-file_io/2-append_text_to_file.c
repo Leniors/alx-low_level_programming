08a3db9a291cb856dd50340a3b64d02bc612982c
Leniors <mokualeeroy@gmail.com>
2023-08-07 09:40:22 -0700

    append text to file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -12,9 +12,12 @@ int append_text_to_file(const char *filename, char *text_content)
 	ssize_t written;
 	int len = 0;
 
-	while (text_content[len] != '\0')
+	if (text_content != NULL)
 	{
-		len++;
+		for (len = 0; text_content[len];)
+		{
+			len++;
+		}
 	}
 
 	if (!filename)
