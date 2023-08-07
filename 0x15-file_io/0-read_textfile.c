bf3f0851537c2d79274f992f17e009cc3a735f6e
Leniors <mokualeeroy@gmail.com>
2023-08-07 05:02:29 -0700

    read textfile

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * read_textfile - reads a text file
+ * @filename: file name
+ * @letters; letters
+ * Return: number of bytes written
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	ssize_t written;
+	ssize_t readen;
+	char *buffer = malloc(letters * sizeof(char));
+
+	if (!filename)
+	{
+		return (0);
+	}
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+	{
+		return (0);
+	}
+	readen = read(fd, buffer, letters);
+	if (readen == -1)
+	{
+		return (0);
+	}
+	written = write(STDOUT_FILENO, buffer, readen);
+	if (written == -1)
+	{
+		return (0);
+	}
+	free(buffer);
+	close (fd);
+	return (written);
+}
