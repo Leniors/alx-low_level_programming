7ea7b1a6211526931aa0413a5dd7c0d24b1ac3e4
Leniors <mokualeeroy@gmail.com>
2023-08-17 05:29:09 -0700

    add elements to linked list

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -17,7 +17,14 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	}
 	new_node->n = n;
 	new_node->prev = NULL;
+	new_node->next = NULL;
+	if (*head == NULL)
+	{
+		*head = new_node;
+		return (*head);
+	}
 	new_node->next = *head;
+	(*head)->prev = new_node;
 	*head = new_node;
 
 	return (*head);
