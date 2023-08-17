6bf1422aa68a2ad381029a1e867f1b2f221ae5f7
Leniors <mokualeeroy@gmail.com>
2023-08-17 04:46:32 -0700

    print elements of linked list

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -11,20 +11,11 @@ size_t print_dlistint(const dlistint_t *h)
 	const dlistint_t *current = h;
 	size_t size = 0;
 
-	if (current == NULL)
-	{
-		return (size);
-	}
-	while (current->next != NULL)
+	while (current != NULL)
 	{
 		printf("%d\n", current->n);
 		size++;
 		current = current->next;
-	}
-	if (current->next == NULL)
-	{
-		printf("%d\n", current->n);
-		size++;
 	}
 
 	return (size);
