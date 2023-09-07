56033a05ef663e30d0653f36a20a9b5c2e62d0f2
Leniors <mokualeeroy@gmail.com>
2023-09-07 21:46:02 +0300

    create hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,8 +9,25 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *ht = malloc(sizeof(hash_table_t));
 
+	if (ht == NULL)
+	{
+		return (NULL);
+	}
+
+	ht->array = malloc(sizeof(hash_node_t) * size);
+
+	if (ht->array == NULL)
+	{
+		return (NULL);
+	}
+
 	ht->size = size;
-	ht->array = malloc(sizeof(hash_node_t));
+
+	for (i = 0; i < size; i++)
+	{
+		ht->array[i] = NULL;
+	}
+
 
 	return (ht);
 }
