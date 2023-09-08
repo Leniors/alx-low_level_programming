128f9cda1aba545b52268411daeb61eba6e869dd
Leniors <mokualeeroy@gmail.com>
2023-09-08 14:51:59 +0300

    delete hash table

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,33 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - deletes a hash table
+ * @ht: hash table
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	hash_node_t *temp, *current;
+	unsigned long int i;
+
+	if (ht == NULL)
+		return;
+
+	for (i = 0; i < ht->size; i++)
+	{
+		if (ht->array[i] == NULL)
+			continue;
+
+		temp = ht->array[i];
+
+		while (temp != NULL)
+		{
+			current = temp;
+			temp = temp->next;
+			free(current->key);
+			free(current->value);
+			free(current);
+		}
+	}
+	free(ht->array);
+	free(ht);
+}
