b7b2f7f778e6050d0cc57518e80b746b5bed5f14
Leniors <mokualeeroy@gmail.com>
2023-10-25 14:39:21 -0700

    object files

---
+++ 0x1C-makefiles/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "school.h"
+
+int main(void)
+{
+	printf("Hello am Main\n");
+	return 0;
+}
---
+++ 0x1C-makefiles/school.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int school(void)
+{
+	printf("Hello am School\n");
+	return 0;
+}
