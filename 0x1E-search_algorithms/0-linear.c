262bf42d9ac3124d4c08a8eb5fef2aa11aa42ddc
Leniors <mokualeeroy@gmail.com>
2024-02-06 22:02:02 +0300

    Linear search

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "search_algos.h"
+
+/**
+ * linear_search - linear search algorithm
+ * @array: array to search on
+ * @size: size of the array
+ * @value: value to search for
+ * Return: returns an integer
+ */
+int linear_search(int *array, size_t size, int value)
+{
+	int i;
+
+	for (i = 0; i < (int)size; i++)
+	{
+		printf("Value checked array[%d] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+		{
+			return i;
+		}
+	}
+	return -1;
+}
---
+++ 0x1E-search_algorithms/README.md
@@ -0,0 +1,1 @@
+ALX search algorithms project
